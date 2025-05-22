808277993a1776a3515b95306db204cac389cb52
Alexandre Pavlovic-Duval <alexandre.pavlovic.duval@gmail.com>
2025-05-22 17:02:20 +0200

    modifying styling

--- screens/FaqScreen.js
+++ screens/FaqScreen.js
@@ -3,6 +3,8 @@ import React from 'react'
 
 export default function FaqScreen() {
   const questionFAQ = "Mon animal s'est fait mordre"
+
+  
   return (
     <View style={styles.mainDiv}>
       <View style={styles.header}>
