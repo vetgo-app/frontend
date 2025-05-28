32eecec85a017ed544a9652ba329593d9039544b
Florian <floriandelaunay75@gmail.com>
2025-05-28 18:33:46 +0200

    ok

--- screens/TakeRdvScreen.js
+++ screens/TakeRdvScreen.js
@@ -48,7 +48,9 @@ export default function TakeRdvScreen({ navigation, route }) {
   useEffect(() => {
     if (!user.token) return;
     fetch(
-      process.env.EXPO_PUBLIC_BACKEND_URL + "/petDocuments/byOwner/" + user.token
+      process.env.EXPO_PUBLIC_BACKEND_URL +
+        "/petDocuments/byOwner/" +
+        user.token
     ).then((response) => response.json().then((data) => setPet(data.data)));
   }, []);
 
